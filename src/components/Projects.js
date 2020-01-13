3220781009915bf75c593bbe967e6d25b8dd2c34
Cezary Sanecki <csanecki@gmail.com>
2020-01-13 16:51:58 +0100

    added projects cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -12,7 +12,67 @@ class Projects extends Component {
     toggleCategories() {
         if(this.state.activeTab === 0) {
             return (
-                <div><h1>React Projects</h1></div>
+                <Grid>
+                    {/* Project 1 */}
+                    <Cell col={ 4 }>
+                        <Card shadow={ 5 } style={{ width: '250px', margin: 'auto'}}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover' }}>
+                                React Project #1
+                            </CardTitle>
+                            <CardText>
+                                lorem
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </Cell>
+
+                    {/* Project 2 */}
+                    <Cell col={ 4 }>
+                        <Card shadow={ 5 } style={{width: '250px', margin: 'auto'}}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover' }}>
+                                React Project #2
+                            </CardTitle>
+                            <CardText>
+                                lorem
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </Cell>
+
+                    {/* Project 3 */}
+                    <Cell col={ 4 }>
+                        <Card shadow={ 5 } style={{width: '250px', margin: 'auto'}}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover' }}>
+                                React Project #3
+                            </CardTitle>
+                            <CardText>
+                                lorem
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </Cell>
+                </Grid>
             )
         } else if(this.state.activeTab === 1) {
             return (
@@ -39,9 +99,7 @@ class Projects extends Component {
                     <Tab>C++</Tab>
                 </Tabs>
 
-                <section className="projects-grid">
-                    { this.toggleCategories() }
-                </section>
+                { this.toggleCategories() }
             </div>
         )
     }
