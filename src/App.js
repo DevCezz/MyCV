1ab82e882a45a9b1dece3ada3704ece8c16b1866
Cezary Sanecki <csanecki@gmail.com>
2020-01-13 09:09:42 +0100

    added base react app with browser routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/Main';
+import { Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello world from React!</h1>
-    </div>
-  );
+    return (
+        <div className="demo-big-content">
+            <Layout fixedHeader>
+                <Header title="Title">
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <Main />
+                </Content>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
