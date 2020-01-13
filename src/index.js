2cbeee85c7175e289eac734b98dd9add3672ac66
Cezary Sanecki <csanecki@gmail.com>
2020-01-13 08:23:48 +0100

    change content of site, add react-mdl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
