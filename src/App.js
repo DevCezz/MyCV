585ea5fd7307ea653013b4f99d502d437bf8450b
Cezary Sanecki <csanecki@gmail.com>
2020-01-16 12:45:55 +0100

    cosmetical changes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/Main';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function App() {
     return (
         <Layout>
-            <Header className="header-color" title={ <Link className="header-main-link" to="/">Home</Link> }>
+            <Header className="header-color" title={ <NavLink className="header-main-link" to="/">Home</NavLink> }>
                 <Navigation>
-                    <Link to="/resume">CV</Link>
-                    <Link to="/aboutme">O mnie</Link>
-                    <Link to="/projects">Projekty</Link>
-                    <Link to="/contact">Kontakt</Link>
+                    <NavLink activeClassName="chosen-page-header" to="/resume">CV</NavLink>
+                    <NavLink activeClassName="chosen-page-header" to="/aboutme">O mnie</NavLink>
+                    <NavLink activeClassName="chosen-page-header" to="/projects">Projekty</NavLink>
+                    <NavLink activeClassName="chosen-page-header" to="/contact">Kontakt</NavLink>
                 </Navigation>
             </Header>
-            <Drawer title={ <Link className="drawer-main-link" to="/">Home</Link> }>
+            <Drawer title={ <NavLink className="drawer-main-link" to="/">Home</NavLink> }>
                 <Navigation>
-                    <Link to="/resume">CV</Link>
-                    <Link to="/aboutme">O mnie</Link>
-                    <Link to="/projects">Projekty</Link>
-                    <Link to="/contact">Kontakt</Link>
+                    <NavLink activeClassName="chosen-page-drawer" to="/resume">CV</NavLink>
+                    <NavLink activeClassName="chosen-page-drawer" to="/aboutme">O mnie</NavLink>
+                    <NavLink activeClassName="chosen-page-drawer" to="/projects">Projekty</NavLink>
+                    <NavLink activeClassName="chosen-page-drawer" to="/contact">Kontakt</NavLink>
                 </Navigation>
             </Drawer>
             <Content>
